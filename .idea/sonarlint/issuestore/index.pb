7810f97153fb3a4e9101428ef98ff7937a295f02
jin <jchj108@naver.com>
2022-07-30 15:40:45 +0900

    상수 대문자 변경

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+Q
+!src/main/java/view/InputView.java,b/2/b2e2cdc2ee9e318d5df660d5ffbf2780b13c300c
+_
+/src/main/java/exception/LackMoneyException.java,1/0/1060506dbc043e588c48589ebb81d4b82e4f20ca
--- src/main/java/domain/AutoLotto.java
+++ src/main/java/domain/AutoLotto.java
@@ -7,16 +7,16 @@ import java.util.List;
 public class AutoLotto {
 
     private final List<LottoTicket> list = new ArrayList<LottoTicket>();
-    public static final long cost = 1000;
+    public static final long COST = 1000;
 
     public List<LottoTicket> getTicket(long payment) {
-        if (payment < cost) {
+        if (payment < COST) {
             throw new IllegalArgumentException("금액이 부족합니다.");
         }
         // while 대신 cost로 나눠서 티켓팅하기
         // while 가독성 문제
-        while (payment >= cost) {
-            payment -= cost;
+        while (payment >= COST) {
+            payment -= COST;
             this.list.add(new LottoTicket());
         }
         return this.list;
