2802f51b6445ed93e062959acd8d775cd0b8f77e
jin <jchj108@naver.com>
2022-07-31 22:46:59 +0900

    LottoNumber Generate시 한번만 생성하도록 변경

--- src/main/java/domain/LottoNumbers.java
+++ src/main/java/domain/LottoNumbers.java
@@ -5,23 +5,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoNumbers {
-
-    private List<LottoNumber> lottoNumberList = new ArrayList<>();
-
-    private static LottoNumbers instance = new LottoNumbers();
-
-    private LottoNumbers() {
+    public List<LottoNumber> lottoNumberList = new ArrayList<>();
+    public LottoNumbers() {
         for (int i = LottoPolicy.START_NUMBER; i < LottoPolicy.END_NUMBER; i++) {
             lottoNumberList.add(new LottoNumber(i));
         }
-    }
-
-    public static LottoNumbers getInstance() {
-        return instance;
-    }
-
-    public List<LottoNumber> getLottoTicket() {
-        Collections.shuffle(this.lottoNumberList);
-        return lottoNumberList.subList(0, 6);
     }
 }
--- src/main/java/domain/LottoTicket.java
+++ src/main/java/domain/LottoTicket.java
@@ -5,14 +5,12 @@ import java.util.*;
 public class LottoTicket {
 
     public List<LottoNumber> autoLottoNumbers;
+    public static LottoNumbers lottoNumbers;
 
     public LottoTicket() {
-        List<LottoNumber> numList = LottoNumbers.getInstance().getLottoTicket();
-//        for (int i = LottoPolicy.START_NUMBER; i <= LottoPolicy.END_NUMBER; i++) {
-//            numList.add(new LottoNumber(i));
-//        }
-        Collections.shuffle(numList);
-        autoLottoNumbers = numList.subList(0, LottoPolicy.LOTTO_NUMBERS_SIZE);
+        LottoNumbers lottoNumbers = new LottoNumbers();
+        Collections.shuffle(lottoNumbers.lottoNumberList);
+        autoLottoNumbers = lottoNumbers.lottoNumberList.subList(0, LottoPolicy.LOTTO_NUMBERS_SIZE);
         Collections.sort(autoLottoNumbers);
     }
 }
