23a4d6de34255f90ce1058e16b03d11ec87bbd9c
jin <jchj108@naver.com>
2022-07-31 17:26:34 +0900

    LottoPrize 삭제

--- src/main/java/domain/LottoPrize.java
+++
@@ -1,9 +0,0 @@
-package domain;
-
-public class LottoPrize {
-
-    private static final long fourthPlace = 5_000;
-    private static final long thirdPlace = 50_000;
-    private static final long twicePlace = 150_000_00;
-    private static final long firstPlace = 2_000_000_000;
-}
