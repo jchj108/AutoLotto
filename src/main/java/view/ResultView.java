ce0929d5d271c829819972a140fdedf95f3a259a
jin <jchj108@naver.com>
2022-07-31 17:24:36 +0900

    ResultView 리팩토링

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -16,11 +16,11 @@ public class ResultView {
         System.out.println(winningNumber.winningNumbers);
         System.out.println("당첨 통계");
         System.out.println("--------");
-        System.out.println("3개 일치 (" + LottoRank.FOURTH.prize + "원) - " + lottoStatistics.lottoWinStatistics.get(3));
-        System.out.println("4개 일치 (" + LottoRank.THIRD.prize + "원) - " + lottoStatistics.lottoWinStatistics.get(4));
-        System.out.println("5개 일치 (" + LottoRank.SECOND.prize + "원) - " + lottoStatistics.lottoWinStatistics.get(5));
-        System.out.println("6개 일치 (" + LottoRank.FIRST.prize + "원) - " + lottoStatistics.lottoWinStatistics.get(6));
-        System.out.println("총 수익률은 " + lottoStatistics.yield + " 입니다." + lottoStatistics.benefit);
+        System.out.println(LottoRank.FOURTH.countOfMatch + "개 일치 (" + LottoRank.FOURTH.prize + "원) - " + lottoStatistics.lottoWinStatistics.get(LottoRank.FOURTH.countOfMatch));
+        System.out.println(LottoRank.THIRD.countOfMatch + "개 일치 (" + LottoRank.THIRD.prize + "원) - " + lottoStatistics.lottoWinStatistics.get(LottoRank.THIRD.countOfMatch));
+        System.out.println(LottoRank.SECOND.countOfMatch + "개 일치 (" + LottoRank.SECOND.prize + "원) - " + lottoStatistics.lottoWinStatistics.get(LottoRank.SECOND.countOfMatch));
+        System.out.println(LottoRank.FIRST.countOfMatch + "개 일치 (" + LottoRank.FIRST.prize + "원) - " + lottoStatistics.lottoWinStatistics.get(LottoRank.FIRST.countOfMatch));
+        System.out.println("총 수익률은 " + lottoStatistics.yield + " 입니다. " + lottoStatistics.benefit);
 
     }
 }
