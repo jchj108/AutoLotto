fdfb050fb07e45c17a9d027a947891df8e647f43
DESKTOP-9JKNRLV\kim <jchj108@naver.com>
2022-08-01 10:38:59 +0900

    테스트 코드 의존성 추가

--- src/test/java/domain/LottoNumberTest.java
+++ src/test/java/domain/LottoNumberTest.java
@@ -1,13 +1,22 @@
 package domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class LottoNumberTest {
 
     @Test
-    void validateNumber() {
+    void 로또_번호_객체_생성() {
+        //Given
+        int randomInt = 5;
+        LottoNumber lottoNumber = new LottoNumber(randomInt);
+        //When
+        //Then
+        assertThat(lottoNumber.getNumber()).isEqualTo(randomInt);
     }
 
     @Test
