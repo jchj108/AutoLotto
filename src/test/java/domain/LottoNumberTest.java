318b79c5176c1cd98efceb813501d8a3a9dfe606
jin <jchj108@naver.com>
2022-08-03 00:03:29 +0900

    junit 의존성 변경, LottoNumberTest 작성

--- src/test/java/domain/LottoNumberTest.java
+++ src/test/java/domain/LottoNumberTest.java
@@ -2,7 +2,10 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,7 +13,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class LottoNumberTest {
 
     @Test
-    void 로또_번호_객체_생성() {
+    @DisplayName("로또 번호를 생성한다")
+    void createLottoNumberTest() {
         //Given
         int randomInt = 5;
         LottoNumber lottoNumber = new LottoNumber(randomInt);
@@ -19,23 +23,22 @@ class LottoNumberTest {
         assertThat(lottoNumber.getNumber()).isEqualTo(randomInt);
     }
 
-    @Test
-    void getNumber() {
+    @DisplayName("범위를 벗어나는 값을 입력하면 Exception을 던진다")
+    @ParameterizedTest(name = "{displayName} ==> notValidInput : {0}")
+    @ValueSource(ints = {-1, 0, 46})
+    void throw_exception_when_out_of_range(int notValidInput) {
+        //Given+When+Then
+        assertThatThrownBy(() -> new LottoNumber(notValidInput))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void setNumber() {
-    }
 
     @Test
-    void compareTo() {
-    }
-
-    @Test
-    void testToString() {
-    }
-
-    @Test
+    @DisplayName("equals 오버라이딩을 테스트한다")
     void testEquals() {
+        LottoNumber lottoNumber1 = new LottoNumber(1);
+        LottoNumber lottoNumber2 = new LottoNumber(1);
+
+        assertEquals(lottoNumber1, lottoNumber2);
     }
 }
